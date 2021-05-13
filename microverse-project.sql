a7c3cc259069c13edf83ea8f78ade757f0a933e3
Barack <barackmukelenga100@gmail.com>
2021-05-13 15:32:56 +0200

    Add missing exemples

--- microverse-project.sql
+++ microverse-project.sql
@@ -615,4 +615,127 @@ FROM teacher;
 
 SELECT t.name, COALESCE(dept.name, 'None')
 FROM teacher t
-LEFT JOIN dept ON t.dept = dept.id
+LEFT JOIN dept ON t.dept = dept.id;
+
+-- Use COUNT to show the number of teachers and the number of mobile phones.
+
+SELECT COUNT(name), COUNT(mobile) FROM teacher;
+
+
+-- Use COUNT and GROUP BY dept.name to show each department and the number of staff. 
+-- Use a RIGHT JOIN to ensure that the Engineering department is listed
+
+SELECT dept.name, COUNT(t.name)
+FROM teacher t 
+RIGHT JOIN dept ON  t.dept = dept.id
+GROUP BY dept.name;
+
+
+-- Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+
+SELECT name, CASE WHEN name IN 
+(SELECT name FROM teacher WHERE dept = 1 OR dept = 2) THEN 'Sci'
+ELSE 'Art'
+END AS name
+FROM teacher;
+
+-- Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in 
+-- dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+
+SELECT name, 
+CASE 
+ WHEN dept IN (1,2) THEN 'Sci'
+ WHEN dept = 3 THEN 'Art'
+ ELSE 'None'
+END 
+FROM teacher;
+
+-- ***** Self join *****
+-- How many stops are in the database.-- 
+
+SELECT COUNT(id) FROM stops;
+
+-- Find the id value for the stop 'Craiglockhart'
+
+SELECT id FROM stops WHERE name = 'Craiglockhart';
+
+-- Give the id and the name for the stops on the '4' 'LRT' service.
+
+SELECT id, name FROM stops 
+JOIN route r ON r.stop = stops.id
+WHERE r.num = '4' AND company = 'LRT';
+
+-- The query shown gives the number of routes that visit either London Road (149)
+--  or Craiglockhart (53). Run the query and notice the two services that link these stops have a count of 2.
+--  Add a HAVING clause to restrict the output to these two routes.
+
+SELECT company, num, COUNT(*)
+FROM route
+WHERE stop = 149 OR stop = 53
+GROUP BY num, company
+HAVING COUNT(*) = 2;
+
+-- Execute the self join shown and observe that b.stop gives all the places you can 
+-- get to from Craiglockhart, without changing routes.
+-- Change the query so that it shows the services from Craiglockhart to London Road.
+
+
+SELECT a.company, a.num, a.stop, b.stop FROM
+	route AS a JOIN route AS b ON (a.company = b.company AND a.num = b.num)
+	WHERE a.stop = 53 AND b.stop = 149;
+    
+-- The query shown is similar to the previous one, however by joining 
+-- two copies of the stops table we can refer to stops by name rather than by number. 
+-- Change the query so that the services between 'Craiglockhart' and 'London Road' are shown.
+-- If you are tired of these places try 'Fairmilehead' against 'Tollcross'
+    
+    SELECT a.company, a.num, astop.name, bstop.name FROM
+	route AS a JOIN route AS b ON (a.company = b.company AND a.num = b.num)
+			   JOIN stops AS astop ON (a.stop = astop.id)
+			   JOIN stops AS bstop ON (b.stop = bstop.id)
+	WHERE astop.name = 'Craiglockhart' AND bstop.name = 'London Road';
+    
+-- Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+    
+SELECT DISTINCT a.company, a.num FROM
+route AS a JOIN route AS b ON (a.company = b.company AND a.num = b.num)
+JOIN stops AS astop ON (a.stop = astop.id)
+JOIN stops AS bstop ON (b.stop = bstop.id)
+WHERE astop.name = 'Haymarket' AND bstop.name = 'Leith'	
+GROUP BY a.num, a.company;
+
+-- Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+
+SELECT DISTINCT a.company, a.num FROM
+route AS a JOIN route AS b ON (a.company = b.company AND a.num = b.num)
+JOIN stops AS astop ON (a.stop = astop.id)
+JOIN stops AS bstop ON (b.stop = bstop.id)
+WHERE astop.name = 'Craiglockhart' AND bstop.name = 'Tollcross'	
+GROUP BY a.num, a.company;
+
+-- Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, 
+-- including 'Craiglockhart' itself, 
+-- offered by the LRT company. Include the company and bus no. of the relevant services.
+
+SELECT DISTINCT bstop.name, a.company, a.num FROM
+	route AS a JOIN route AS b ON (a.company = b.company AND a.num = b.num)
+			   JOIN stops AS astop ON (a.stop = astop.id)
+			   JOIN stops AS bstop ON (b.stop = bstop.id)
+	WHERE astop.name = 'Craiglockhart';
+    
+-- Find the routes involving two buses that can go from Craiglockhart to Lochend.
+-- Show the bus no. and company for the first bus, the name of the stop for the transfer,
+-- and the bus no. and company for the second bus.
+   
+SELECT v1.r1num, v1.r1com, v1.trans1, v2.r4num, v2.r4com
+FROM (SELECT s1.name AS begin, r1.num AS r1num, r1.company AS r1com,       s2.name AS trans1
+      FROM route r1 JOIN route r2 ON r1.company = r2.company AND r1.num = r2.num
+      JOIN stops s1 ON s1.id = r1.stop
+      JOIN stops s2 ON s2.id = r2.stop
+      WHERE s1.name = 'Craiglockhart') AS v1
+     JOIN(SELECT s3.name AS trans2, r4.num AS r4num, r4.company AS r4com, s4.name AS final
+     FROM route r3 JOIN route r4 ON r3.company = r4.company AND r3.num = r4.num
+     JOIN stops s3 ON s3.id = r3.stop
+     JOIN stops s4 ON s4.id = r4.stop
+     WHERE s4.name = 'Lochend') AS v2 ON v1.trans1 = v2.trans2
+ORDER BY r1num, trans1, r4num
